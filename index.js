c9ad2f2f54f75b0f2d1d2041f24892a77aeeb23e
EstebanMagallonPerez <esteban1995perez@gmail.com>
2017-06-23 13:24:41 -0700

    starting point of the million man march

---
+++ index.js
@@ -0,0 +1,33 @@
+/*
+IOS DEPENDANCIES/SETUP
+	- enable SSH over WiFi https://www.reddit.com/r/jailbreak/comments/5s19qg/tutorial_ssh_over_wifi_with_yalu102_jb/
+	- install SQLite 3.x from Cydia/Telesphoreo repo
+	- install CLSMS from https:// s1ris.github.io/repo
+*/
+var fs 					= require('fs');
+var express 			= require('express');
+var http 				= require('http');
+var app 				= express();
+var server 				= http.createServer(app);
+var io 					= require('socket.io').listen(server);
+
+
+app.use(express.static(__dirname + '/html'));
+
+app.get('/', function(req, res){
+	res.sendfile('./index.html');
+});
+
+io.on('connection', function(socket){
+
+
+	socket.on('newCount', function(data){
+
+	});
+	console.log("connected");
+
+});
+
+server.listen(3000, function(){
+	console.log('nodeJS Messages is now online');
+});
