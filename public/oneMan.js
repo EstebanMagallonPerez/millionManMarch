46f8765c64b6e6cbbd8480b7fa2b8c11272729cd
EstebanMagallonPerez <esteban1995perez@gmail.com>
2017-06-25 04:01:52 -0700

    updated worker so the worker can now use a server sent function

--- public/oneMan.js
+++ public/oneMan.js
@@ -1,4 +1,7 @@
-self.addEventListener('message',function(e){
-	var men = parseInt(e.data);
-	postMessage(men++);
+self.addEventListener('message',function(data){
+	//this is the data passed by the server
+	var parsed = data.data[0];
+	//We store the function that the server sent into a function variable so we can execute it
+	eval("var temp =" + parsed.userFunc)
+	temp();
 })
