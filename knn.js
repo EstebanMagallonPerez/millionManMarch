f8fb0018a34384ddddcb58b981d5c1f2f1efecfd
EstebanMagallonPerez <esteban1995perez@gmail.com>
2017-06-27 13:50:56 -0700

    cleaned things up a little bit

--- knn.js
+++ knn.js
@@ -11,26 +11,17 @@ app.use(express.static(__dirname + '/public'));
 app.get('/', function(req, res){
 	res.sendfile('./public/index.html');
 });
-var men = 0;
-var sendInfo;
-
-
-
-
 var dataFileBuffer  = fs.readFileSync(__dirname + '/mnist/images.idx3-ubyte');
 var labelFileBuffer = fs.readFileSync(__dirname + '/mnist/labels.idx1-ubyte');
-var pixelValues     = [];
-
-// It would be nice with a checker instead of a hard coded 60000 limit here
-var testSetImages = [];
-var testSetLables = [];
-var trainigSetImages = [];
-var trainigSetLables = [];
-var classifiedImage = 10;//10 - 10,000
-var unclassifiedImage = 0;//0-10
+var classifiedImage = 10;	//10 - 10,000
+var unclassifiedImage = 0;	//0-10
+var numNeighbors = 5;
 
 var start = new Date().getTime();
 
+
+//This gets an image from our mnist file and returns an array with:
+//[label, array of pixels, the index of this image]
 function getImage(image)
 {
 	var pixels = [];
@@ -42,81 +33,76 @@ function getImage(image)
 	var imageData = [labelFileBuffer[image + 8],pixels,image];
 	return imageData;
 }
+
+//This function is passed to the user, and the result is returned in the format
+//[distance, index of unclassified, index of classified, classification]
+//image 1 is not classified, image 2 is
 function getDifference(){
-	var unClassified = data.data[0][0];
-	var classified = data.data[0][1];
-	var sum = 0;
-	for(var i = 0; i < unClassified[1].length;i++)
+	var image1 = data.data[0][0];
+	var image2 = data.data[0][1];
+	var distance = 0;
+	for(var i = 0; i < image1[1].length;i++)
 	{
-		sum += Math.abs(unClassified[1][i] - classified[1][i]);
+		distance += Math.abs(image1[1][i] - image2[1][i]);
 	}
-	//result,index of unclassified,index of classified,the current classification;
-	output = [sum,unClassified[2],classified[2],classified[0]];
-	postMessage(output);
-	//console.log("the difference is: ");
-	//console.log(sum);
+	output = [distance,image1[2],image2[2],image2[0]];
 	return output;
 }
-var classifications = [
-	{"image":0,"classification":-1,"nearest":[{"classification":-1,"distance":200000},{"classification":-1,"distance":200000},{"classification":-1,"distance":200000},{"classification":-1,"distance":200000},{"classification":-1,"distance":200000}]},
-	{"image":1,"classification":-1,"nearest":[{"classification":-1,"distance":200000},{"classification":-1,"distance":200000},{"classification":-1,"distance":200000},{"classification":-1,"distance":200000},{"classification":-1,"distance":200000}]},
-	{"image":2,"classification":-1,"nearest":[{"classification":-1,"distance":200000},{"classification":-1,"distance":200000},{"classification":-1,"distance":200000},{"classification":-1,"distance":200000},{"classification":-1,"distance":200000}]},
-	{"image":3,"classification":-1,"nearest":[{"classification":-1,"distance":200000},{"classification":-1,"distance":200000},{"classification":-1,"distance":200000},{"classification":-1,"distance":200000},{"classification":-1,"distance":200000}]},
-	{"image":4,"classification":-1,"nearest":[{"classification":-1,"distance":200000},{"classification":-1,"distance":200000},{"classification":-1,"distance":200000},{"classification":-1,"distance":200000},{"classification":-1,"distance":200000}]},
-	{"image":5,"classification":-1,"nearest":[{"classification":-1,"distance":200000},{"classification":-1,"distance":200000},{"classification":-1,"distance":200000},{"classification":-1,"distance":200000},{"classification":-1,"distance":200000}]},
-	{"image":6,"classification":-1,"nearest":[{"classification":-1,"distance":200000},{"classification":-1,"distance":200000},{"classification":-1,"distance":200000},{"classification":-1,"distance":200000},{"classification":-1,"distance":200000}]},
-	{"image":7,"classification":-1,"nearest":[{"classification":-1,"distance":200000},{"classification":-1,"distance":200000},{"classification":-1,"distance":200000},{"classification":-1,"distance":200000},{"classification":-1,"distance":200000}]},
-	{"image":8,"classification":-1,"nearest":[{"classification":-1,"distance":200000},{"classification":-1,"distance":200000},{"classification":-1,"distance":200000},{"classification":-1,"distance":200000},{"classification":-1,"distance":200000}]},
-	{"image":9,"classification":-1,"nearest":[{"classification":-1,"distance":200000},{"classification":-1,"distance":200000},{"classification":-1,"distance":200000},{"classification":-1,"distance":200000},{"classification":-1,"distance":200000}]}
-]
+//Here we define our storage method
+//There are 10 testing values, and the number of neighbors will change based on the preset value
+var classifications = [];
+for(let i = 0; i < 10; i++)
+{
+	var neighbors = [];
+	for(var j = 0; j < numNeighbors;j++)
+	{
+		neighbors.push({"classification":-1,"distance":200000});
+	}
+	classifications.push({"image":i,"classification":-1,"nearest":neighbors})
+}
+
+//Preparing the data to be sent to the client
+//NOTE that for some reason objects cannot be sent. all the data was lost when I tried that.
+//The function that send must be converted into a string... at least that seems to be the only thing that worked for me
+//
 function getDataToSend()
 {
-	//Sample value to emit
-	//The data must be in an array, not an object so we are able to parse the value later
-	//The userFunction is converted to a string before we send it to the user
 	var unclassifeid = getImage(unclassifiedImage);
 	var classified = getImage(classifiedImage);
 	data = [unclassifeid,classified];
-	//var userFunction = function(){console.log("data is: ");console.log(data.data[0]);}
 	passFunc = ''+getDifference;
-	sendInfo = [data,passFunc];
-	if(classifiedImage == 100)
+	var toSend = [data,passFunc];
+	if(classifiedImage == 110)
 	{
-		classifiedImage = 0;
+		classifiedImage = 10;
 		console.log("test image is: "+unclassifiedImage+" true class is: "+ labelFileBuffer[unclassifiedImage + 8])
-		for(var i =0;i<classifications[unclassifiedImage].nearest.length;i++)
-		{
-			console.log(classifications[unclassifiedImage].nearest[i].classification);
-		}
-		//console.log(classifications[unclassifiedImage].nearest[i]);
+		console.log(classifications[unclassifiedImage].nearest);
 		unclassifiedImage ++;
-
-	}
-	if(unclassifiedImage > 9)
-	{
-		sendInfo = -1;
-		var end = new Date().getTime();
-		var time = end - start;
-		console.log('Execution time: ' + time);
+		if(unclassifiedImage == 10)
+		{
+			sendInfo = -1;
+			var end = new Date().getTime();
+			var time = end - start;
+			console.log('Execution time: ' + time);
+			return -1;
+		}
 	}
 	classifiedImage++;
+	return toSend;
 }
 
 io.on('connection', function(socket){
-	getDataToSend();
-	if(sendInfo == -1){
+	var toSend =  getDataToSend();
+	if(toSend == -1){
 		return;
 	}
-	socket.emit('number', sendInfo); // We send some information to our user, for now it is a value. We also need a function
+	socket.emit('number', toSend);
 
 	socket.on('sendBackToServer', function(output){
-		//Here you put your check to determine if the client is sending something useful
-		//console.log(output);
 		for(var i = 0; i < classifications[output[1]].nearest.length;i++)
 		{
 			var max = classifications[output[1]].nearest[0].distance;
 			var maxIndex = 0;
-
 			for (var i = 1; i < classifications[output[1]].nearest.length; i++) {
 				if (classifications[output[1]].nearest[i].distance > max) {
 					maxIndex = i;
@@ -129,13 +115,12 @@ io.on('connection', function(socket){
 				classifications[output[1]].nearest[maxIndex].classification = output[3]
 			}
 		}
-		getDataToSend();
-		//io.sockets.emit('updatedMen',men);
-		if(sendInfo == -1){
+		toSend = getDataToSend();
+		if(toSend == -1){
 			return;
 		}
-		socket.emit('number', sendInfo);
-	}); // listen to the event
+		socket.emit('number', toSend);
+	});
 	console.log("connected");
 
 });
--- public/oneMan.js
+++ public/oneMan.js
@@ -1,8 +1,4 @@
 self.addEventListener('message',function(data){
-	//this is the data passed by the server
-//	console.log(data.data[0]);
-//	var parsed = data.data[0];
-	//We store the function that the server sent into a function variable so we can execute it
-	eval("var temp =" + data.data[1])
+	eval("var temp =" + data.data[1]);//evalueate the function sent by the server
 	postMessage(temp());
 })
