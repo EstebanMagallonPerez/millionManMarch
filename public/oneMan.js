495c2d3821d85bdc91936ab9837eec5c2aecc3e1
EstebanMagallonPerez <estebanmagallonperez@gmail.com>
2020-08-27 15:22:29 -0700

    fix bug causing creation of worker on every number

--- public/oneMan.js
+++ public/oneMan.js
@@ -1,4 +1,4 @@
 self.addEventListener('message',function(data){
-	eval("var temp =" + data.data[1]);//evalueate the function sent by the server
+	eval("var temp =" + data.data[1]);//evaluate the function sent by the server
 	postMessage(temp());
 })
